af65b090127b54db206a61886e11345ed616893e
edvardwd <edvard.wdingsor@gmail.com>
2025-03-04 15:01:24 +0100

    added ifndef etc.

--- source/Timer.h
+++ source/Timer.h
@@ -1,3 +1,5 @@
+#ifndef TIMER_H
+#define TIMER_H
 #include <time.h>
 
 typedef struct{
@@ -11,3 +13,4 @@ void initTimer(Timer* timer);
 void startTimer(Timer* timer);
 void stopTimer(Timer* timer);
 int getTimePassed(Timer* timer);
+#endif
